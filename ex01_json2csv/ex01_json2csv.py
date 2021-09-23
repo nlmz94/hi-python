32597fdae6f0de9cfb45d0fa214431aa5586be56
Nael Mezdari <naelmez18@gmail.com>
2021-09-23 09:30:54 +0200

    added a non-pandas version for ex01

--- ex01_json2csv/ex01_json2csv.py
+++ ex01_json2csv/ex01_json2csv.py
@@ -1,6 +1,30 @@
+from re import escape
 import pandas as pd
+import json
+import csv
 
 
-def json_to_csv(file_path):
+def json_to_csv_with_pandas(file_path):
     new_data = pd.read_json(file_path)
     new_data.to_csv("generated_csv.csv", index=None)
+
+
+def json_to_csv_without_pandas(file_path):
+    with open(file_path) as file_json:
+        json_data = json.load(file_json)
+    new_csv_file = open('generated_csv_not_pandas.csv', 'wt')
+    csv_writer = csv.writer(
+        new_csv_file, escapechar='\n', quoting=csv.QUOTE_NONE)
+    top_row = ''
+    for key in json_data.keys():
+        top_row += key + ';'
+    csv_writer.writerow([top_row])
+    for key in json_data.keys():
+        normal_row = ''
+        value_row = ''
+        for sub_key in json_data[key].keys():
+            normal_row += sub_key.strip(' ') + ';'
+            value_row += json_data[key][sub_key].strip(' ') + ';'
+        csv_writer.writerow([normal_row.strip(' ')])
+        csv_writer.writerow([value_row.strip(' ')])
+    new_csv_file.close()
--- ex01_json2csv/main.py
+++ ex01_json2csv/main.py
@@ -1,10 +1,12 @@
 import sys
 
-from ex01_json2csv import json_to_csv
+from ex01_json2csv import json_to_csv_with_pandas
+from ex01_json2csv import json_to_csv_without_pandas
 
 
 def main_json_to_csv():
-    json_to_csv(sys.argv[1])
+    # json_to_csv_with_pandas(sys.argv[1])
+    json_to_csv_without_pandas(sys.argv[1])
 
 
 if __name__ == '__main__':
