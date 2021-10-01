1ac614c2972a8c53d803fe2e7eeebdb1d55f7982
naelmez <nmz94140@gmail.com>
2021-10-01 09:34:54 +0200

    better rps

--- ex03_rps/ex03_rps.py
+++ ex03_rps/ex03_rps.py
@@ -5,6 +5,8 @@ def play_rock_paper_scissors_against_computer():
     user_input = input("enter r for rock, p for paper and s for scissors : ")
     computer_input = random.choice(['r', 'p', 's'])
     print(f'Inputs : player : {user_input} and computer : {computer_input}')
+    if user_input not in ['r', 'p', 's']:
+        return 'Incorrect input'
     if user_input == computer_input:
         return 'It\'s a tie'
     if (user_input == 'r' and computer_input == 's') or (user_input == 's' and computer_input == 'p') or (
