7bc97b5d8e26ec26c0eeefa1dd2a8f81f82ba6e2
naelmez <nmz94140@gmail.com>
2021-10-01 10:30:16 +0200

    better json to csv

--- ex01_json2csv/ex01_json2csv.py
+++ ex01_json2csv/ex01_json2csv.py
@@ -1,7 +1,6 @@
 from re import escape
 import pandas as pd
 import json
-import csv
 
 
 def json_to_csv_with_pandas(file_path):
@@ -10,21 +9,13 @@ def json_to_csv_with_pandas(file_path):
 
 
 def json_to_csv_without_pandas(file_path):
-    with open(file_path) as file_json:
-        json_data = json.loads(file_json.read())
-    new_csv_file = open('generated_csv_not_pandas.csv', 'wt')
-    csv_writer = csv.writer(
-        new_csv_file, escapechar='\n', quoting=csv.QUOTE_NONE)
-    top_row = ''
-    for key in json_data.keys():
-        top_row += key + ';'
-    csv_writer.writerow([top_row])
-    for key in json_data.keys():
-        normal_row = ''
-        value_row = ''
-        for sub_key in json_data[key].keys():
-            normal_row += sub_key.strip(' ') + ';'
-            value_row += json_data[key][sub_key].strip(' ') + ';'
-        csv_writer.writerow([normal_row.strip(' ')])
-        csv_writer.writerow([value_row.strip(' ')])
-    new_csv_file.close()
+    with open(file_path) as file:
+        filedictionary = json.load(file)
+        fileheader = ''
+        filedata = ''
+        for key, item in filedictionary.items():
+            fileheader += key + ';'
+            filedata += json.dumps(item).strip() + ';'
+        with open('generated_csv_not_pandas.csv', 'w', newline='') as csvfile:
+            csvfile.write(fileheader + '\n')
+            csvfile.write(filedata + '\n')
