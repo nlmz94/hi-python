3e6ae2f14e0cee4bdd19f6ad5db6eecd22dff8e3
Nael Mezdari <nmz94140@gmail.com>
2022-02-10 09:49:38 +0100

    better

--- ex01_json2csv/ex01_json2csv.py
+++ ex01_json2csv/ex01_json2csv.py
@@ -1,6 +1,6 @@
-from re import escape
-import pandas as pd
 import json
+
+import pandas as pd
 
 
 def json_to_csv_with_pandas(file_path):
@@ -10,12 +10,12 @@ def json_to_csv_with_pandas(file_path):
 
 def json_to_csv_without_pandas(file_path):
     with open(file_path) as file:
-        filedictionary = json.load(file)
-        fileheader = ''
-        filedata = ''
-        for key, item in filedictionary.items():
-            fileheader += key + ';'
-            filedata += json.dumps(item).strip() + ';'
+        file_dictionary = json.load(file)
+        file_header = ''
+        file_data = ''
+        for key, item in file_dictionary.items():
+            file_header += key + ';'
+            file_data += json.dumps(item).strip() + ';'
         with open('generated_csv_not_pandas.csv', 'w', newline='') as csvfile:
-            csvfile.write(fileheader + '\n')
-            csvfile.write(filedata + '\n')
+            csvfile.write(file_header + '\n')
+            csvfile.write(file_data + '\n')
