f6b0bbd6a141035e3bf1812e9d3c4d7c15998922
Nael Mezdari <nmz94140@gmail.com>
2022-02-10 09:37:14 +0100

    loop

--- ex03_rps/ex03_rps.py
+++ ex03_rps/ex03_rps.py
@@ -13,3 +13,18 @@ def play_rock_paper_scissors_against_computer():
             user_input == 'p' and computer_input == 'r'):
         return 'Player wins'
     return 'Computer wins'
+
+
+def play_rock_paper_scissors_gameloop():
+    user_score = 0
+    ia_score = 0
+    while user_score < 3 and ia_score < 3:
+        round_winner = play_rock_paper_scissors_against_computer()
+        print(round_winner)
+        if round_winner == 'Player wins':
+            user_score = user_score + 1
+        if round_winner == 'Computer wins':
+            ia_score = ia_score + 1
+    if user_score == 3:
+        return 'Player wins the game!'
+    return 'Computer wins the game!'
--- ex03_rps/main.py
+++ ex03_rps/main.py
@@ -1,9 +1,9 @@
 import sys
-from ex03_rps import play_rock_paper_scissors_against_computer
+from ex03_rps import play_rock_paper_scissors_gameloop
 
 
 def main():
-    print(play_rock_paper_scissors_against_computer())
+    print(play_rock_paper_scissors_gameloop())
 
 
 if __name__ == '__main__':
